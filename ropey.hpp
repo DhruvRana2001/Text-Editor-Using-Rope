aa7c3ad06f7663fbfc742ad88b4dac505cdf044f
Aditya Kesari <53120349+ayetida21@users.noreply.github.com>
2024-04-14 15:00:47 -0400

    Update ropey.hpp

--- ropey.hpp
+++ ropey.hpp
@@ -6,6 +6,10 @@ QT_BEGIN_NAMESPACE
 namespace Ui {
 class Ropey;
 }
+class QAction;
+class QMenu;
+class QPlainTextEdit;
+class QSessionManager;
 QT_END_NAMESPACE
 
 class Ropey : public QMainWindow
@@ -16,6 +20,25 @@ public:
     Ropey(QWidget *parent = nullptr);
     ~Ropey();
 
+    void loadFile(const QString &fileName);
+
+private slots:
+    void newFile();
+    void open();
+    bool save();
+    bool saveAs();
+    void close();
+
+
 private:
     Ui::Ropey *ui;
+    void setupStatusTips();
+    void setupShortcuts();
+    void setupConnections();
+
+    bool saveFile(const QString &fileName);
+    void setCurrentFile(const QString &fileName);
+    QString strippedName(const QString &fullFileName);
+
+    QString curFile;
 };
